4bf552cea1ecda3d05fce570fbb5c3fe577b6b6a
GMkonan <guilhemont@hotmail.com>
2024-04-24 08:21:37 -0300

    Initial design for email

--- src/components/emails/SubscribeEmailTemplate.tsx
+++ src/components/emails/SubscribeEmailTemplate.tsx
@@ -4,6 +4,13 @@ import {
   Heading,
   Button,
   Link,
+  Head,
+  Font,
+  Row,
+  Hr,
+  Section,
+  Text,
+  Body,
 } from "@react-email/components";
 import * as React from "react";
 
@@ -19,18 +26,60 @@ export const MyEmail: React.FC<Readonly<EmailTemplateProps>> = ({
 }) => {
   return (
     <Html>
-      <Container>
-        <Heading as="h1">Affinity suite is on sale!</Heading>
-        <Button
-          href="https://affinity.serif.com/affinity-pricing/"
-          style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
-        >
-          Head to their website
-        </Button>
-        <Link href={`http://localhost:3000/api/unsubscribe?email=${userEmail}`}>
-          Unsubscribe from email notifications
-        </Link>
-      </Container>
+      <Head>
+        <Font
+          fontFamily="Roboto"
+          fallbackFontFamily="Verdana"
+          webFont={{
+            url: "https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+            format: "woff2",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
+      {/* style={{ background: "#fafbfb" }} */}
+      <Body>
+        {/* border: "1px solid #E5E5E5" */}
+        <Container style={{ textAlign: "center" }}>
+          <Section>
+            <Heading as="h1">Affinity suite is on sale!</Heading>
+          </Section>
+          <Section style={{ paddingBottom: "12px" }}>
+            <Row>
+              <Button
+                href="https://affinity.serif.com/affinity-pricing/"
+                style={{
+                  background: "#0377bf",
+                  color: "#fff",
+                  padding: "12px 20px",
+                  borderRadius: "4px",
+                  margin: "6px",
+                }}
+              >
+                Head to their website
+              </Button>
+            </Row>
+          </Section>
+          <Row>
+            <Text style={{ color: "#787676" }}>
+              You are receinving this because you have subscribed to receive
+              notifications when Affinity Suite is on sale. If you received this
+              email by mistake, please unsubscribe by clicking the link below.
+            </Text>
+          </Row>
+          <Hr style={{ paddingBottom: "12px" }} />
+          <Link
+            href={`http://localhost:3000/api/unsubscribe?email=${userEmail}`}
+            // style={{
+            //   textDecorationColor: "#dfe5e9",
+            //   color: "#dfe5e9",
+            // }}
+          >
+            Unsubscribe
+          </Link>
+        </Container>
+      </Body>
     </Html>
   );
 };
